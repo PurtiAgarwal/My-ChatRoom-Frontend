33a832da71b99499ff8fe20da4f735710adcb9f5
purtiagarwal <purtiagarwal88@gmail.com>
2022-07-31 19:33:08 +0530

    adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MainForm from "./component/MainForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ChatRoom from "./component/ChatRoom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <div
+            className="container-fluid bg-light text-dark d-flex align-items-center justify-content-center"
+            style={{ height: "100vh" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Router>
+                <Routes>
+                    <Route index element={<MainForm />} />
+                    <Route path="chat/:roomId" element={<ChatRoom />} />
+                    <Route path="*" element={<h1>404 not found !</h1>} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
